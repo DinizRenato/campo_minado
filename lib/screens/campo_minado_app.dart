8052d33dc8047edd5d88ce9aab53d8da458ce44e
Renato Diniz <dinizre@hotmail.com>
2020-04-14 20:22:08 -0300

    CampoWidget

--- lib/screens/campo_minado_app.dart
+++ lib/screens/campo_minado_app.dart
@@ -1,23 +1,41 @@
 import 'package:flutter/material.dart';
 
 import '../components/resultado_widget.dart';
+import '../components/campo_widget.dart';
+import '../models/campo.dart';
+import '../models/explosao_exception.dart';
 
 class CampoMinadoApp extends StatelessWidget {
-
-  _reiniciar(){
+  
+  void _reiniciar() {
     print('Reiniciar...');
+  }
+
+  void _abrir(Campo campo){
+    print('Abrir...');
+  }
+  
+  void _alternarMarcacao(Campo campo){
+    print('Alternar marcacao...');
   }
 
   @override
   Widget build(BuildContext context) {
+
+    Campo campo = new Campo(linha: 0, coluna: 0);
+
     return MaterialApp(
       home: Scaffold(
-        appBar:ResultadoWidget(
-          venceu : true,
+        appBar: ResultadoWidget(
+          venceu: true,
           onReiniciar: _reiniciar,
         ),
         body: Container(
-          child: Text('Tabuleiro'),
+          child: CampoWidget(
+            campo: campo,
+            onAbrir: _abrir,
+            onAlternarMarcacao: _alternarMarcacao,
+          ),
         ),
       ),
     );
